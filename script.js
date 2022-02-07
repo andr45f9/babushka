4341ee8d3810aca7063eb33a63d15f0024545510
Andrea Schack <andr45f9@stud.kea.dk>
2022-02-07 19:30:56 +0100

    styling samt menu

--- script.js
+++ script.js
@@ -8,23 +8,42 @@ const options = {
   },
 };
 
+let data;
+let filter = "alle";
+
+const filterKnapper = document.querySelectorAll("nav button");
+filterKnapper.forEach((knap) => knap.addEventListener("click", filtrerMad));
+
+function filtrerMad() {
+  filter = this.dataset.kategori;
+
+  document.querySelector(".valgt").classList.remove("valgt");
+  this.classList.add("valgt");
+
+  vis(data);
+}
+
 async function hentData() {
   const resspons = await fetch(url, options);
   const json = await resspons.json();
-  vis(json);
+
+  data = json;
+
+  vis(data);
 }
 
-function vis(json) {
-  console.log(json);
+function vis(data) {
+  container.textContent = "";
 
-  json.forEach((mad) => {
-    const klon = template.cloneNode(true);
-    klon.querySelector("h2").textContent = mad.navn;
-    klon.querySelector("img").src = "medium/" + mad.billednavn + "-md.jpg";
-    klon.querySelector(".info").textContent = mad.kortbeskrivelse;
-    klon.querySelector(".pris").textContent = mad.pris + " kr.";
-
-    container.appendChild(klon);
+  data.forEach((mad) => {
+    if (filter == mad.kategori || filter == "alle") {
+      const klon = template.cloneNode(true);
+      klon.querySelector("h2").textContent = mad.navn;
+      klon.querySelector("img").src = "medium/" + mad.billednavn + "-md.jpg";
+      klon.querySelector(".info").textContent = mad.kortbeskrivelse;
+      klon.querySelector(".pris").textContent = mad.pris + " kr.";
+      container.appendChild(klon);
+    }
   });
 }
 
